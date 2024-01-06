e12a4c8983649a43bf210a0950ccf714fbd4800e
jenia <jenia8513@gmail.com>
2024-01-06 17:16:38 +0500

    fix schemas Pet

--- src/schemas/pet_schema.py
+++ src/schemas/pet_schema.py
@@ -1,5 +1,11 @@
+import datetime
+
 from pydantic import BaseModel
 from typing import Union
+
+
+class PetSupport(BaseModel):
+    id: str
 
 
 class PetCreate(BaseModel):
@@ -9,17 +15,32 @@ class PetCreate(BaseModel):
     photo: list
 
 
-class PetResponse(BaseModel):
+class PetCreateResponse(PetCreate, PetSupport):
+    created_at: datetime.datetime
+
+
+class PetGet(BaseModel):
     limit: Union[int, None] = 20
     offset: Union[int, None] = 0
     has_photo: Union[bool, None] = None
+
+
+class PetGetResponse(BaseModel):
+    count: int
+    items: list
 
 
 class PetDelete(BaseModel):
     ids: list
 
 
-class PhotoCreate(BaseModel):
-    id: int
+class PetDeleteResponse(PetDelete):
+    pass
+
+
+class PhotoCreate(PetSupport):
     file: bytes
 
+
+class PhotoCreateResponse(PetSupport):
+    url: str
