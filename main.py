66d5ec93ce2fcd6446650182c15f6318a204269a
jenia <jenia8513@gmail.com>
2024-01-07 21:54:02 +0500

    add main

---
+++ main.py
@@ -0,0 +1,25 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from routes import get_apps_router
+
+
+def get_application() -> FastAPI:
+    application = FastAPI()
+    application.include_router(get_apps_router())
+
+    application.add_middleware(
+        CORSMiddleware,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    return application
+
+
+app = get_application()
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="localhost", reload=True)
